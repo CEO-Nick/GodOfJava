6bd7712f591f5a7f603af1641f7401e1695ccb54
IkHeyonCho <andantej281@gmail.com>
2024-02-19 09:43:50 +0900

    자바의 신2 - 2장.자바 랭 클래스에 대해

---
+++ src/Chapter02_JavaLang/doitself/NumberObjects.java
@@ -0,0 +1,27 @@
+package Chapter02_JavaLang.doitself;
+
+public class NumberObjects {
+    public static void main(String[] args) {
+        NumberObjects sample = new NumberObjects();
+        sample.parseLong("1024");
+        sample.parseLong("d1024");
+        sample.printOtherBase(1024);
+    }
+
+    public long parseLong(String data) {
+        try {
+            long result = Long.parseLong(data);
+            return result;
+        } catch (NumberFormatException e) {
+            System.out.println(data + " is not a number.");
+            return -1;
+        }
+    }
+
+    public void printOtherBase(long value) {
+        System.out.println("Original : " + value);
+        System.out.println("Binary : " + Long.toBinaryString(value));
+        System.out.println("Hex : " + Long.toHexString(value));
+        System.out.println("Octal : " + Long.toOctalString(value));
+    }
+}
